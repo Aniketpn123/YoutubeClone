6d164e35edf2e51f47c88df46d7fcd82e81dfcc1
Aniket <aniketpn123@gmail.com>
2025-10-16 20:31:59 +0530

    e

--- JS/amazon.js
+++ JS/amazon.js
@@ -1,42 +1,11 @@
-const products = [
-  {
-    img: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    price: 1090,
-  },
-  {
-    img: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    price: 2095,
-  },
-  {
-    img: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    price: 799,
-  },
-];
-
-
-let productsHTML='';
+let productsHTML = '';
 products.forEach((product) => {
-  productsHTML+= `
+  productsHTML += `
    <div class="product-container">
           <div class="product-image-container">
             <img
               class="product-image"
-              src="${product.img }"
+              src="${product.image}"
             />
           </div>
 
@@ -47,12 +16,14 @@ products.forEach((product) => {
           <div class="product-rating-container">
             <img
               class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars*10}.png"
+              src="images/ratings/rating-${product.rating.stars * 10}.png"
             />
-            <div class="product-rating-count link-primary">${product.rating.count}</div>
+            <div class="product-rating-count link-primary">${
+              product.rating.count
+            }</div>
           </div>
 
-          <div class="product-price">${(product.price/100).toFixed(2)}</div>
+          <div class="product-price">${(product.priceCents / 100).toFixed(2)}</div>
 
           <div class="product-quantity-container">
             <select>
@@ -78,7 +49,6 @@ products.forEach((product) => {
 
           <button class="add-to-cart-button button-primary">Add to Cart</button>
         </div> `;
-      
 });
 
-document.querySelector('.js-producst-grid').innerHTML=productsHTML;
+document.querySelector('.js-producst-grid').innerHTML = productsHTML;
