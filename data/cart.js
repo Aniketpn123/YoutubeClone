8b0d354f305e93e491665ddfcc587d7bf6f4627f
Aniket <aniketpn123@gmail.com>
2025-10-17 22:37:52 +0530

    checkout js

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,10 @@
 export const cart = [
   {
-    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c',
+    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     qty:2,
   },
   {
-    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3',
+    productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
     qty:1,
   }
 ];
